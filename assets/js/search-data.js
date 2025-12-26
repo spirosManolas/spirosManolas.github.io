e9bcaad37c0bb59281eb18a7ac5423f6e2d613fd
spirosManolas <spirosmanolas99@gmail.com>
2025-12-26 00:50:10 +0000

    Deploying to gh-pages from @ spirosManolas/spirosManolas.github.io@0bd353b90a8586d8c0dad60a9e570a09e0f832ee 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -14,7 +14,7 @@ ninja.data = [{
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/%5Cassets%5Cpdf%5Csm_cv.pdf";
+            window.location.href = "/assets/pdf/sm_cv.pdf";
           },
         },{id: "post-a-post-with-plotly-js",
         
